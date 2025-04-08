7595788e068a95c0deb94d1458a33eee58920d99
vivanco44 <alejandromvm4@gmail.com>
2025-04-08 16:25:55 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+// main.js
+const { exec } = require('child_process');
+const startMiddleware = require('./middleware');
+
+// Iniciar middlewares
+startMiddleware(3001);
+startMiddleware(3002);
+
+// Iniciar HAProxy en el puerto 2000 usando un archivo de configuración
+exec('haproxy -f haproxy.cfg', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error al iniciar HAProxy: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`HAProxy iniciado:\n${stdout}`);
+});
