1bbe74cd1ddfdd42e6d7c802352c0a48ddbc263a
vivanco44 <alejandromvm4@gmail.com>
2025-04-10 16:36:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ const { exec } = require('child_process');
 const startMiddleware = require('./middleware');
 
 
-require('./broker'); // Esto inicia el broker en el puerto 6000
-
 // Iniciar middlewares
 startMiddleware(3001);
 startMiddleware(3002);
