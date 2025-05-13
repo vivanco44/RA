d497da1066c9b4a2739ff5718969e58164ea2986
vivanco44 <alejandromvm4@gmail.com>
2025-05-13 15:57:01 +0200

    Update broker.js

--- broker.js
+++ broker.js
@@ -1,4 +1,4 @@
-// broker.js
+
 const aedes = require('aedes')();
 const net = require('net');
 
@@ -7,16 +7,16 @@ const PORT = 6000;
 const server = net.createServer(aedes.handle);
 
 server.listen(PORT, () => {
-  console.log(`📡 Broker MQTT corriendo en el puerto ${PORT}`);
+  console.log(` Broker MQTT corriendo en el puerto ${PORT}`);
 });
 
 aedes.on('client', client => {
-  console.log(`🔌 Cliente conectado: ${client ? client.id : 'desconocido'}`);
+  console.log(` Cliente conectado: ${client ? client.id : 'desconocido'}`);
 });
 
 aedes.on('publish', (packet, client) => {
   if (client) {
-    console.log(`📨 Mensaje recibido de ${client.id}:`);
+    console.log(`Mensaje recibido de ${client.id}:`);
     console.log(`   → Topic: ${packet.topic}`);
     console.log(`   → Payload: ${packet.payload.toString()}`);
   }
