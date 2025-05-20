23e627ea978668daba8e3a28659e6cca6233cafb
vivanco44 <alejandromvm4@gmail.com>
2025-05-20 17:22:21 +0200

    Update load.js

--- load.js
+++ load.js
@@ -1,7 +1,4 @@
 const axios = require('axios');
-const CryptoJS = require('crypto-js');
-
-const secretKey = 'mi_clave_secreta'; // Misma clave que en middleware
 
 // Número de solicitudes a enviar
 const numRequests = 100;
@@ -9,17 +6,12 @@ const numRequests = 100;
 // Función para generar datos aleatorios
 function generateRandomData() {
   return {
-    id_nodo: `Nav${Math.floor(Math.random() * 10) + 1}`,
+    id_nodo: Nav${Math.floor(Math.random() * 10) + 1},
     temperatura: Math.floor(Math.random() * 35) + 15,
     humedad: Math.floor(Math.random() * 50) + 30,
     co2: Math.floor(Math.random() * 500) + 200,
     volatiles: Math.floor(Math.random() * 10) + 1
   };
-}
-
-// Función para cifrar los datos con AES
-function encryptData(data) {
-  return CryptoJS.AES.encrypt(JSON.stringify(data), secretKey).toString();
 }
 
 // Retardo de n milisegundos
@@ -30,16 +22,15 @@ function delay(ms) {
 // Función principal asincrónica
 async function enviarSolicitudes() {
   for (let i = 0; i < numRequests; i++) {
-    const rawData = generateRandomData();
-    const payload = encryptData(rawData);
+    const data = generateRandomData();
 
     try {
-      const response = await axios.post('http://localhost:2000/record', { payload }, {
+      const response = await axios.post('http://localhost:2000/record', data, {
         headers: { 'Content-Type': 'application/json' }
       });
-      console.log(`(${i + 1}/${numRequests}) ✅ POST cifrado enviado:`, response.status);
+      console.log((${i + 1}/${numRequests}) ✅ POST exitoso:, response.status);
     } catch (error) {
-      console.error(`(${i + 1}/${numRequests}) ❌ Error en POST:`, error.message);
+      console.error((${i + 1}/${numRequests}) ❌ Error en POST:, error.message);
     }
 
     await delay(1000); // Espera 1 segundo antes del siguiente POST
