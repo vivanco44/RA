9f3d8b9f4fb178c80598c775f381247567657147
vivanco44 <alejandromvm4@gmail.com>
2025-05-20 17:23:59 +0200

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -7,7 +7,7 @@ function startMiddleware(port) {
   const mqttClient = mqtt.connect('mqtt://localhost:6000'); // Conexión al broker
 
   mqttClient.on('connect', () => {
-    console.log(🔗 Middleware en puerto ${port} conectado al broker MQTT);
+    console.log(Middleware en puerto ${port} conectado al broker MQTT);
   });
 
   app.use(express.json());
