e70817b478466de4ed1a13cfdcb6e4aa32225b7f
vivanco44 <alejandromvm4@gmail.com>
2025-05-22 16:33:12 +0200

    Update middleware_blacklist.js

--- middleware_blacklist.js
+++ middleware_blacklist.js
@@ -14,20 +14,20 @@ function startBlacklistMiddleware() {
     const cleanIP = ip.replace(/^::ffff:/, '');
 
     if (isBlacklisted(cleanIP)) {
-      console.warn(chalk.yellowBright(`[Blacklist] Petición bloqueada desde IP prohibida: ${cleanIP}`));
+      console.warn(`[Blacklist] Petición bloqueada desde IP prohibida: ${cleanIP}`);
       return res.status(403).json({ error: 'Acceso denegado desde esta IP' });
     }
 
-    console.log(chalk.greenBright(`[Blacklist] IP autorizada: ${cleanIP}, reenviando a balanceador (${target})`));
+    console.log(`[Blacklist] IP autorizada: ${cleanIP}, reenviando a balanceador (${target})`);
 
     proxy.web(req, res, { target }, (err) => {
-      console.error(chalk.redBright(`[Blacklist] Error al reenviar al balanceador: ${err.message}`));
+      console.error(`[Blacklist] Error al reenviar al balanceador: ${err.message}`);
       res.status(500).send('Error interno del proxy de blacklist');
     });
   });
 
   app.listen(5000, () => {
-    console.log(chalk.redBright('[Blacklist] Middleware de filtrado escuchando en el puerto 5000'));
+    console.log('[Blacklist] Middleware de filtrado escuchando en el puerto 5000');
   });
 }
 
